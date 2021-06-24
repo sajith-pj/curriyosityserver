bee05c4848a20cdf10dff7ccabcb4bba04c8f835
sajith <sajithsanju@gmail.com>
2021-06-25 01:23:55 +0530

    setting up the hosting environment

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if( process.env.NODE_ENV === 'production')
 {
   app.use(express.static('Curriosity/build'))
   app.use(express.static('curryosity-admin/build'))
-  app.get('/',(req, res)=>{
+  app.get('/*',(req, res)=>{
     res.sendFile(path.join( __dirname, 'Curriosity','build','index.html' ))
   })
   app.get('/admin',(req, res)=>{
