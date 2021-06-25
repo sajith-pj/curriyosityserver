7334f35376ce8742082b09090819e418720ff01e
sajith <sajithsanju@gmail.com>
2021-06-25 22:16:35 +0530

    ADDED CORS POLICY

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ var app = express();
 var db = require('./config/connection')
 var session = require('express-session')
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors( {origin: '*'}))
 db.connect((err)=>{
   if(err)
   console.log("error"+err);
