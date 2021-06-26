63f972cf291d1663fc9789e943a430440c9049ff
sajith <sajithsanju@gmail.com>
2021-06-26 14:07:46 +0530

    redirects

--- app.js
+++ app.js
@@ -3,46 +3,55 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var cors = require('cors')
+var cors = require('cors');
 var adminRouter = require('./routes/admin');
 var usersRouter = require('./routes/users');
 
 var app = express();
-var db = require('./config/connection')
-var session = require('express-session')
+var db = require('./config/connection');
+var session = require('express-session');
 
-app.
+app.use(cors({ origin: 'https://restaurantuser.herokuapp.com', credentials: true, exposedHeaders: ['set-cookie'] }));
+app.use(
+	session({
+		secret: 'curiyosity',
+		resave: false,
+		saveUninitialized: false,
+		cookie: { maxAge: 600000, httpOnly: true, sameSite: 'none', secure: true },
+	})
+);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+db.connect((err) => {
+	if (err) console.log('error' + err);
+	else console.log('database connected');
+});
 
-db.connect((err)=>{
-  if(err)
-  console.log("error"+err);
-  else
-  console.log("database connected");
-})
-
-app.use((req, res, next)=> {
-  res.cookie("check","123",{ 
-    sameSite: "lax"
-   })
-})
+app.use((req, res, next) => {
+	res.cookie('check', '123', {
+		sameSite: 'lax',
+	});
+});
 app.use('/admin', adminRouter);
 app.use('/', usersRouter);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error'+ err);
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error' + err);
 });
 
 module.exports = app;
