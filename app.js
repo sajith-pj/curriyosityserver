6f82b7072c0452348560d4e3ce29220b0980ce32
sajith <sajithsanju@gmail.com>
2021-06-26 13:16:27 +0530

    redirects

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({origin:"https://restaurantuser.herokuapp.com", credentials:true,exposedHeaders:["set-cookie"]}))
-app.use(session({  secret:"curiyosity",cookie:{maxAge:600000, secure:true,sameSite:'none'}}));
+app.use(session({  secret:"curiyosity",cookie:{maxAge:600000,sameSite:'lax'}}));
 db.connect((err)=>{
   if(err)
   console.log("error"+err);
