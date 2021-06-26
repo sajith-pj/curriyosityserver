eecb370a5ad502c2d6950b7daa34fef7d8308887
sajith <sajithsanju@gmail.com>
2021-06-26 06:34:52 +0530

    redirects

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+app.use(cors({origin:'https://restaurantuser.herokuapp.com', credentials:true}))
 db.connect((err)=>{
   if(err)
   console.log("error"+err);
