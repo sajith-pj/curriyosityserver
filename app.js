f4ef3072c74b84f98ae0d8870870773a1e9c0bd3
sajith <sajithsanju@gmail.com>
2021-06-26 06:20:27 +0530

    redirects

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 var db = require('./config/connection')
 var session = require('express-session')
 
-app.use(cors({crendentials:true}))
+app.use(cors({ origin:'https://restaurantuser.herokuapp.com',crendentials:true}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
